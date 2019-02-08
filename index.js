d6460127ce79416f083ddf7c6ef1984f141def0d
Juha Myllari <jmyllari@gmail.com>
2019-02-08 16:36:11 +0200

    prepare for Heroku

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app = express()
 const MAX_ID_FACTOR = 1e9
+const cors = require('cors')
 const bodyParser = require('body-parser')
 var morgan = require('morgan')
-
 
 app.use(bodyParser.json())
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 let persons = [
     {
