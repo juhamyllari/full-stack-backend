d6274bbf5a25b4f875f11da9e3f1774be0e8443a
Juha Myllari <jmyllari@gmail.com>
2019-02-08 17:33:56 +0200

    add frontend build

---
+++ build/precache-manifest.da0e292340b5184b58a5f766a1c8fa7b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "efc606f8fec2b18ec64d",
+    "url": "/static/js/main.efc606f8.chunk.js"
+  },
+  {
+    "revision": "23feea3aedfdbea1a84a",
+    "url": "/static/js/1.23feea3a.chunk.js"
+  },
+  {
+    "revision": "e6205efcb59c46fbb354b15b5590524e",
+    "url": "/index.html"
+  }
+];
